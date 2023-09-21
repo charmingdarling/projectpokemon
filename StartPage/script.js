5f3a8f93d6eb0e1d6f388ee396939799e7f582fb
RahimmullahSaboorudin <RahimmullahSaboorudin@gmail.com>
2023-09-20 22:53:38 -0400

    Start Screen

---
+++ StartPage/script.js
@@ -0,0 +1,41 @@
+function startGame() {
+    alert('Are you sure you want to make a new Game?');
+}
+function continueGame() {
+    alert('Continue Game?');
+}
+// Code below will add a sound when pressing a button
+
+ // Function to play the "goal1.mp3" sound
+ function playGoalSound() {
+    var audio = new Audio('goal1.mp3');
+    audio.play();
+  }
+
+  // Function to start a new game
+  function startGame() {
+  
+    // Play the sound when the button is clicked
+    playGoalSound();
+  }
+
+  // Function to continue the game
+  function continueGame() {
+ 
+    // Play the sound when the button is clicked
+    playGoalSound();
+  }
+  //Background music for website
+  var backgroundMusic = document.getElementById("backgroundMusic");
+  var musicPlaying = true;
+
+  function playBackgroundMusic() {
+    setTimeout(function () {
+      if (musicPlaying) {
+        backgroundMusic.pause();
+      } else {
+        backgroundMusic.play();
+      }
+      musicPlaying = !musicPlaying;
+    }, 2500); // Delay the play action by 2.5 seconds (2500 milliseconds)
+  }
