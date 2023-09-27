f8b5be203ec206404f33641205b7919822daa50c
charmingdarling <h1hasinai_f06@yahoo.com>
2023-09-26 17:47:06 -0700

    Linking views of handlebars to proper routes. Created separate logout and login. I think that the routes for users work and as well for server side.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -24,7 +24,9 @@ router.post('/register', async (req, res) => {
 // *Login //
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { username: req.body.username } });
+    const userData = await User.findOne({
+      where: { email: req.body.email },
+    });
 
     if (!userData) {
       res
@@ -36,9 +38,7 @@ router.post('/login', async (req, res) => {
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect password, please try again' });
+      res.status(400).json({ message: 'Incorrect password, please try again' });
       return;
     }
 
